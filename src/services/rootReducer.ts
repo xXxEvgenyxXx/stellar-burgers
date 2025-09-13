c773c9b409776061a029719e8cef8c2eec600542
Evgeny Simonov <jdsimonov2@gmail.com>
2025-09-13 19:02:33 +0300

    Revert "	modified:   src/services/rootReducer.ts"
    This reverts commit 8b240c1a90ca21f87450f987636378d39f2a47bb.

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -1,14 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import ingredientsReducer from './slices/ingredientsSlice';
-import userReducer from './slices/userSlice';
-import feedReducer from './slices/feedSlice';
-import ordersReducer from './slices/ordersSlice';
 
 const rootReducer = combineReducers({
-  ingredients: ingredientsReducer,
-  user: userReducer,
-  feed: feedReducer,
-  orders: ordersReducer
+  ingredients: ingredientsReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
--- src/services/slices/userSelectors.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from '../store';
-
-export const selectUser = (state: RootState) => state.user.user;
-export const selectIsAuthChecked = (state: RootState) => state.user.isAuthChecked;
-export const selectUserLoading = (state: RootState) => state.user.loading;
-export const selectUserError = (state: RootState) => state.user.error;
