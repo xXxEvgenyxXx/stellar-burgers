4bd3ec3e8d8256bbd9bd57ce461868ab96762342
Evgeny Simonov <jdsimonov2@gmail.com>
2025-09-15 21:29:24 +0300

    modified:   src/components/ui/app-header/app-header.tsx

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -8,12 +8,15 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import { NavLink } from 'react-router-dom';
+
 export type TAppHeaderUIProps = {
   userName?: string;
   onConstructorClick?: () => void;
   onFeedClick?: () => void;
   onProfileClick?: () => void;
 };
+
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ 
   userName, 
   onConstructorClick, 
@@ -23,22 +26,32 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <div 
-          className={styles.menu_item} 
-          onClick={onConstructorClick}
-          style={{ cursor: 'pointer' }}
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => 
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
         >
-          <BurgerIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </div>
-        <div 
-          className={styles.menu_item} 
-          onClick={onFeedClick}
-          style={{ cursor: 'pointer' }}
+          {({isActive})=>(
+            <>
+              <BurgerIcon type={isActive ? 'primary':'secondary'} />
+              <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
+            </>
+          )}
+        </NavLink>
+        <NavLink 
+          to="/feed" 
+          className={({ isActive }) => 
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
         >
-          <ListIcon type={'primary'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </div>
+          {({isActive})=>(
+            <>
+              <ListIcon type={isActive ? 'primary':'secondary'} />
+              <p className='text text_type_main-default ml-2'>Лента заказов</p>
+            </>
+          )}
+        </NavLink>
       </div>
       <div className={styles.logo}>
         <Logo className='' />
