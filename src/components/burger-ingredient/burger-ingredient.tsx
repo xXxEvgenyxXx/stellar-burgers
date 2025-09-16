616305dcb99399de0647edf8b81485c14daed01d
Evgeny Simonov <jdsimonov2@gmail.com>
2025-09-16 22:14:51 +0300

    modified:   src/components/burger-ingredient/burger-ingredient.tsx

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable */
 import { FC, memo } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useDispatch } from '../../services/store';
+import { useDispatch, useSelector } from '../../services/store';
 import { addIngredient, setBun } from '../../services/slices/burgerConstructorSlice';
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
@@ -12,6 +12,14 @@ export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
     const location = useLocation();
     const dispatch = useDispatch();
+    
+    // Получаем ингредиенты из конструктора
+    const constructorItems = useSelector((state: any) => state.burgerConstructor.constructorItems);
+    
+    // Вычисляем количество данного ингредиента в конструкторе
+    const ingredientCount = constructorItems.ingredients.filter(
+      (item: TConstructorIngredient) => item._id === ingredient._id
+    ).length + (constructorItems.bun?._id === ingredient._id ? 2 : 0);
 
     const handleAdd = () => {
       console.log('handleAdd сработал');
@@ -32,7 +40,7 @@ export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
     return (
       <BurgerIngredientUI
         ingredient={ingredient}
-        count={count}
+        count={ingredientCount > 0 ? ingredientCount : undefined}
         locationState={{ background: location }}
         handleAdd={handleAdd}
       />
