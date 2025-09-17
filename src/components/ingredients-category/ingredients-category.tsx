caba6f26d61ed687d5253a8dd8e3c277379ac598
Evgeny Simonov <jdsimonov2@gmail.com>
2025-09-17 16:19:09 +0300

    modified:   src/components/ingredients-category/ingredients-category.tsx

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -1,4 +1,7 @@
+/* prettier-ignore */
+/* eslint-disable */
 import { forwardRef, useMemo } from 'react';
+import { useSelector } from '../../services/store';
 import { TIngredientsCategoryProps } from './type';
 import { TIngredient } from '@utils-types';
 import { IngredientsCategoryUI } from '../ui/ingredients-category';
@@ -8,12 +11,7 @@ export const IngredientsCategory = forwardRef<
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
   /** TODO: взять переменную из стора */
-  const burgerConstructor = {
-    bun: {
-      _id: ''
-    },
-    ingredients: []
-  };
+  const burgerConstructor = useSelector((state: any) => state.burgerConstructor.constructorItems);
 
   const ingredientsCounters = useMemo(() => {
     const { bun, ingredients } = burgerConstructor;
