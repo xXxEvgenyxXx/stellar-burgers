a76b33cd97eea617db5c936d95c6d4ea10cdcff2
Evgeny Simonov <jdsimonov2@gmail.com>
2025-09-24 11:26:57 +0300

    modified:   src/components/ingredients-category/ingredients-category.tsx

--- src/components/ingredients-category/ingredients-category.tsx
+++ src/components/ingredients-category/ingredients-category.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable */
 import { forwardRef, useMemo } from 'react';
 import { useSelector } from '../../services/store';
+import { RootState } from '../../services/store';
 import { TIngredientsCategoryProps } from './type';
 import { TIngredient } from '@utils-types';
 import { IngredientsCategoryUI } from '../ui/ingredients-category';
@@ -11,12 +12,12 @@ export const IngredientsCategory = forwardRef<
   TIngredientsCategoryProps
 >(({ title, titleRef, ingredients }, ref) => {
   /** TODO: взять переменную из стора */
-  const burgerConstructor = useSelector((state: any) => state.burgerConstructor.constructorItems);
+  const burgerConstructor = useSelector((state: RootState) => state.burgerConstructor.constructorItems);
 
   const ingredientsCounters = useMemo(() => {
-    const { bun, ingredients } = burgerConstructor;
+    const { bun, ingredients: constructorIngredients } = burgerConstructor;
     const counters: { [key: string]: number } = {};
-    ingredients.forEach((ingredient: TIngredient) => {
+    constructorIngredients.forEach((ingredient: TIngredient) => {
       if (!counters[ingredient._id]) counters[ingredient._id] = 0;
       counters[ingredient._id]++;
     });
